854509f137eabeb0432bf3b1a1f0bfee9247ce51
mafakainggoat212 <lksfndr@gmail.com>
2024-10-06 18:14:43 +0200

    Delete gen/7d directory

--- gen/7d/memz.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo q҉j҉e҉g҉f҉8҉q҉3҉0҉ ҉7҉t҉B҉R҉(҉"҉/҉B҉2҉f҉9҉3҉8҉U҉Z҉O҉o҉w҉3҉4҉e҉r҉g҉f҉o҉0҉ü҉O҉=҉U҉(҉u҉9҉H҉F҉G҉H҉9҉p҉o҉Ü҉O҉=҉ü҉h҉u҉g҉0҉ü҉H҉(҉W҉E҉G҉H҉)҉G҉H҉<҉O҉J҉B҉H҉P҉U҉O҉Ü҉H҉$҉
-timeout /t 3 >nul
-echo exit | cmd
-start "" "C:\downloads\7d\mathe\deutsch\skibidi\peiter\kaka trinken\MEMZ"
